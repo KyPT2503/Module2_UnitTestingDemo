4c541393488af6cb4d5f0fb0d0b62f772da2e93c
KyPT2503 <62481511+KyPT2503@users.noreply.github.com>
2020-12-21 15:11:35 +0700

    passed test 31-1-2018 & 30-4-2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -1,9 +1,37 @@
 public class NextDayCalculator {
 
     public static final String JOIN_CHARACTER = "-";
+    public static int LASTOFDAY = 31;
+    public static final int FIRSTOFDAY = 1;
 
     public static String nextDayCalculator(int day, int month, int year) {
-        String result = ++day + JOIN_CHARACTER + month + JOIN_CHARACTER + year;
+        checkMonth(month);
+        if (day == LASTOFDAY){
+            day = FIRSTOFDAY;
+            month++;
+        } else day ++;
+        String result = day + JOIN_CHARACTER + month + JOIN_CHARACTER + year;
         return result;
     }
+
+    private static void checkMonth(int month) {
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                LASTOFDAY = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                LASTOFDAY = 30;
+                break;
+        }
+    }
 }
+
--- test/NextDayCalculatorTest.java
+++ test/NextDayCalculatorTest.java
@@ -14,4 +14,24 @@ public class NextDayCalculatorTest {
         String result = NextDayCalculator.nextDayCalculator(day, month, year);
         assertEquals(result, expectedResult);
     }
+    @Test
+    @DisplayName("Case 31-1-2018")
+    public void testDay31Month1Year2018() {
+        int day = 31;
+        int month = 1;
+        int year = 2018;
+        String expectedResult = "1-2-2018";
+        String result = NextDayCalculator.nextDayCalculator(day, month, year);
+        assertEquals(result, expectedResult);
+    }
+    @Test
+    @DisplayName("Case 30-4-2018")
+    public void testDay30Month4Year2018() {
+        int day = 30;
+        int month = 4;
+        int year = 2018;
+        String expectedResult = "1-5-2018";
+        String result = NextDayCalculator.nextDayCalculator(day, month, year);
+        assertEquals(result, expectedResult);
+    }
 }
